ac1c0a4ec85f821dc461fbde2d47675a3f951a35
Adam <adam@example.com>
2021-08-22 11:13:54 -0600

    initial structure

---
+++ src/components/content/Content.js
@@ -0,0 +1,13 @@
+import PromoAd from "./PromoAd";
+import UpdateProfileName from "./UpdateProfileName";
+
+const Content = () => {
+  return (
+    <>
+      <PromoAd />
+      <UpdateProfileName />
+    </>
+  );
+};
+
+export default Content;
